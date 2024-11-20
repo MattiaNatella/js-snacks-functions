2bbbf4f36a6055f625c7d944b1893ea593af2ee4
MattiaNatella <mattianatella@gmail.com>
2024-11-20 15:22:39 +0100

    second exercise

--- snack-funzioni/02 - Filtra iniziali/script.js
+++ snack-funzioni/02 - Filtra iniziali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const alfabeto = prompt('inserisci una lettera')
 
 // Dichiara la funzione qui.
-function 
-
+function ricerca(arrayStringhe, lettera) {
+  let arrayFiltrato = [];
+  for(let i = 0; i < arrayStringhe.length; i++) {
+   if (arrayStringhe[i].includes(lettera)) {
+    arrayFiltrato.push(arrayStringhe[i])
+    }                                      
+  }
+  return arrayFiltrato
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(ricerca(names, alfabeto))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
